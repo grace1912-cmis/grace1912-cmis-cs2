65c8862d66bdfaa4d2ef245cadb7b62a9a71d488
cmis <cmis@cmis-hs11.(none)>
2016-01-27 11:13:39 +0700

    Saved my file

---
+++ hello.py
@@ -0,0 +1,13 @@
+print "Hello,world"
+print "I hate you"
+print "This is hard"
+print "I'm confused"
+print True
+print False
+print 77
+print "Inhye Kim"
+print "student"
+print ("Inhye",1912,"student")
+print (7.7, 7, 777, "my", "favorite", "number") 
+print ("food", "is","life")
+print ("#12", "blush")
