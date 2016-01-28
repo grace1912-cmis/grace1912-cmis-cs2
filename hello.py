e672d8ae7ddb27e30112a11683c4add40083c5f0
cmis <cmis@cmis-hs11.(none)>
2016-01-28 14:34:22 +0700

    Saved the file

--- hello.py
+++ hello.py
@@ -11,3 +11,17 @@ print ("Inhye",1912,"student")
 print (7.7, 7, 777, "my", "favorite", "number") 
 print ("food", "is","life")
 print ("#12", "blush")
+myName = "Innhye"
+print 777
+myAge = 77777
+print "I'm", "done"
+n = 7*7
+print n
+m = 8 % 2
+print m
+m = 10 % 2
+print m
+m = 7 % 5
+print m
+x = 10 // 2
+print x
