9b17e12fe099c38d3359b54fdf1cb92e92e9fd01
cmis <cmis@cmis-hs11.(none)>
2016-02-01 09:56:27 +0700

    saved the file

---
+++ assignment1.py
@@ -0,0 +1,23 @@
+myName ="Inhye Kim"
+print myName
+ageInYears = 16.9
+print ageInYears
+heightInMeters = 1.66
+print heightInMeters
+lengthOfSquare = 8
+print lengthOfSquare
+lengthOfRectangle = 16
+print lengthOfRectangle
+heightOfRectangle = 2
+print heightOfRectangle
+monthsInYear = 12
+ageInMonths = ageInYears * monthsInYear
+print ageInMonths
+yearsLeftToLive = 100
+yearsToLive = ageInYears + yearsLeftToLive
+print yearsToLive
+heightInFeet = ageInYears * 3.28084
+print heightInFeet
+averageHeight= 5.73 *3.28084
+differenceOfHeight= 
+print differenceOfHeight
