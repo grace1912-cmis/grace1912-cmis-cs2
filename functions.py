00c7ee5a5176430222f3b1eb5cdb21cf891f1f90
cmis <cmis@cmis-hs11.(none)>
2016-02-09 14:06:56 +0700

    followed all the steps

---
+++ functions.py
@@ -0,0 +1,34 @@
+#Added two arguments
+def add(a,b):
+    return a + b
+y = add(7,7)
+print y
+#Subtracted two arguments
+def sub(a,b):
+    return a - b
+x= sub(8,4)
+print x
+#Multiplied two arguments
+def mul(a,b):
+    return a*b
+n= mul(9,2)
+print n
+#Divided two arguments
+def div(a,b):
+    return a/b
+f= div(4,2)
+print f
+#Conversion of hours to seconds
+def hours_from_seconds_div(a,b):
+    return a/b
+s= div(86400,3600)
+print s
+#Representation of a radius of a circle with one argument
+def circle_area(r):
+    return 3.14159265359 * (r**2)
+print circle_area(5)
+def sphere_volume(v):
+    return 3.14159265359 * 1.333333333333 * (v**3)
+print sphere_volume(5)
+
+
