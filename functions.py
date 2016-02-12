1fbe250e75a00207409a54b1aac158aac524313a
cmis <cmis@cmis-hs11.(none)>
2016-02-12 11:55:44 +0700

    Saved my files

--- functions.py
+++ functions.py
@@ -27,17 +27,33 @@ print s
 def circle_area(r):
     return 3.14159265359 * (r**2)
 print circle_area(5)
+#Representation of a volume of the spere
 def sphere_volume(v):
     return 3.14159265359 * 1.333333333333 * (v**3)
 print sphere_volume(5)
-def sphere_volume1 (r):
-    return 3.14159265359 * 1.333333333333 * (r**3)
-print sphere_volume1(10/2)
-def sphere_volume2(r):
-    return 3.14159265359 *1.333333333333 * (r**3)
-print sphere_volume2(20/2)
+#Representation of the average of the volumes
 def avg_volume(a,b):
-    return a + b
-a= add 
+    return ((1.0/6 * 3.14159265359 * a**3) + (1.0/6 * 3.14159265359 * b**3)) /2
+print avg_volume (10,20)
+#Area of the three side lengths of a triangle
+def area(a,b,c):
+	n=(a+b+c)/2
+	return (n*(n-a)*(n-b)*(n-c))**0.5
+
+print area(1,2,2.5)
+#String as an argument tht returns that word with additional spaces
+def right_align(word):
+	return (80-len(word))* " " + word
+print right_align( "Hello" )
+#String as an argument tht returns the spaces centered on the screen
+def center(word):
+    return (40-len(word))*" "+word
+print center("Hello")
+#Message Box
+def msg_box(word):
+    return "+" + (len(word)) +4 * "-") + "+"
+
+
+
 
 
