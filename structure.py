5929fa0d101193b3cb61dfd5cd6b61aaf15787fc
cmis <cmis@cmis-hs11.(none)>
2016-03-04 14:35:34 +0700

    saved

--- structure.py
+++ structure.py
@@ -1,22 +1,21 @@
 import math
 def exp(e, x):
     return e ** x
-def output (name, e, x, y):
+def volumeOfsphere(v):
+	volume = (4/3) * math.pi * (v ** 3)
+def totals(a, b, c):
+	return a + b + c
+def output (a, b, c, d):
     out = """
-Hey {} !
-Did you know:
-{} ** {} = {}
-""".format(name, e, x, y)
+There are three spheres on the table. Sphere One has a volume of {}, Sphere Two has a volume of {}, and Sphere Three has a volume of {}. The total volume of three Spheres are {}.
+""".format(a, b, c, d)
     return out
 
 def main():
-   
-    name = raw_input("What is your name? : ")
-    e= int(raw_input("Type a number: "))
-    x= int(raw_input("Type another: "))
-    y= exp(e, x)
-
-    out = output(name, e, x, y)
-    print out
-
+	a= int(raw_input("The radius of SphereOne : "))
+	b= int(raw_input("The radius of SphereTwo: "))
+	c= int(raw_input("The radius of SphereThree: "))
+	d= totals(a, b, c)
+	out = output(a, b, c, d)
+	print out
 main()
