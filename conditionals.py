800ac54bce10e6c0a299c0bac5a3e12adf96149f
cmis <cmis@cmis-hs01.(none)>
2016-03-24 14:05:20 +0700

    Saved

--- conditionals.py
+++ conditionals.py
@@ -1,2 +1,18 @@
-#You are trapped in a Pizza Restaurant. Certain types of pizzas can let you out. Your task is to figure out the right type of pizza to be free from the Pizza place.
+#You are trapped in a Pizza Restaurant. Certain types of pizzas can let you out. Your task is to figure out the right type of pizza to be freed from the Pizza place. You have four sections to choose the right pizza. You only have three choices for each sections. Your choices have points. Greater the better. 
+
+
+print ("Holly macaroni! You are trapped in a Pizza Restaurent! If you don't want to end your life with pathetic pizzas, GO ahead and figure out the KEY PIZZA to get out of this traumatic place! Good Luck (:")
+import math
+import random
+
+Isocelese==int(15)
+Scalene==int(10)
+Right==int(5)
+def GuessingShape():
+    Shape= raw_input("Choose the type of triangles of your pizza(Isoceles, Scalene, Right): ")
+
+    if Shape< Scalene:
+    Shape= random.randint(int(Isoceles, Scalene,Right))
+    ScaleneValue= int(10)
+    return ScaleneValue
 
