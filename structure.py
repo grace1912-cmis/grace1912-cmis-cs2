9edd2f9b00ae5ce22abda3434f9015e42f2230fb
cmis <cmis@cmis-hs01.(none)>
2016-03-29 09:55:47 +0700

    edited

--- structure.py
+++ structure.py
@@ -2,27 +2,34 @@ import math
 def exp(e, x):
     return e ** x
 def volumeOfsphere(v):
-	volume = (4/3) * math.pi * (v ** 3)
-def totals(a, b, c):
-	return a + b + c
-def output (VolumeOne, VolumeTwo, VolumeThree, d):
+	volume = (4/3) * math.pi * (int(v) ** 3)
+def volumeone(a):
+	return float(a)
+def volumeone(b):
+	return float(b)
+def volumeone(c):
+	return float(c)
+def totals(a,b,c):
+	return (a+b+c)
+def output (a,b,c,d):
     out = """
 There are three spheres on the table. Sphere One has a volume of {}, Sphere Two has a volume of {}, and Sphere Three has a volume of {}. The total volume of three Spheres are {}.
-""".format(VolumeOne,VolumeTwo ,VolumeThree, d)
+""".format(a,b,c,d)
     return out
-
+#Input
 def main():
 	a= int(raw_input("The radius of SphereOne : "))
 	b= int(raw_input("The radius of SphereTwo: "))
 	c= int(raw_input("The radius of SphereThree: "))
-	d= totals(VolumeOne, VolumeTwo, VolumeThree)
+#Processing
 
-VolumeOne=int(volumeOfsphere())
-VolumeTwo=int(volumeOfsphere(VolumeTwo))
-VolumeThree=int(volumeOfsphere(VolumeThree))
+	VolumeOne=volumeOfsphere(a)
+	VolumeTwo=volumeOfsphere(b)
+	VolumeThree=volumeOfsphere(c)
+	d= totals(VolumeOne, VolumeTwo, VolumeThree)	
+#Output
+	out = output(VolumeOne, VolumeTwo, VolumeThree, d)
 
-out = output (VolumeOne, VolumeTwo, VolumeThree, d)
 
-
-print out
+	print out
 main()
