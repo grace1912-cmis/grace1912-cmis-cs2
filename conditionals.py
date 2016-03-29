0d6aeb93f974accfd46031fa276a749c4433681a
cmis <cmis@cmis-hs01.(none)>
2016-03-29 09:56:31 +0700

    editedd

--- conditionals.py
+++ conditionals.py
@@ -1,18 +1,28 @@
-#You are trapped in a Pizza Restaurant. Certain types of pizzas can let you out. Your task is to figure out the right type of pizza to be freed from the Pizza place. You have four sections to choose the right pizza. You only have three choices for each sections. Your choices have points. Greater the better. 
+#You are trapped in a Pizza Restaurant. Certain types of pizzas can let you out. Your task is to figure out the right type of pizza to be freed from the Pizza place. You have four sections to choose the right pizza. You only have three choices for each sections. You need to get 10 points to get out.
 
-
-print ("Holly macaroni! You are trapped in a Pizza Restaurent! If you don't want to end your life with pathetic pizzas, GO ahead and figure out the KEY PIZZA to get out of this traumatic place! Good Luck (:")
+print ("Holly macaroni! You are trapped in a Pizza Restaurent! If you don't want to end your life with pathetic pizzas, GO ahead and figure out the KEY PIZZA to get out of this traumatic place! Make sure to get maxiGood Luck (:")
 import math
 import random
 
-Isocelese==int(15)
-Scalene==int(10)
-Right==int(5)
 def GuessingShape():
     Shape= raw_input("Choose the type of triangles of your pizza(Isoceles, Scalene, Right): ")
 
-    if Shape< Scalene:
-    Shape= random.randint(int(Isoceles, Scalene,Right))
-    ScaleneValue= int(10)
-    return ScaleneValue
+if Shape == "Scalene"
+	print OnePoint
+elif Shape == "Isoceles"
+	print TwoPoints
+elif Shape == "Right"
+	print ThreePoints
+def GuessingType():
+	Type= raw_input("Choose the type of of your pizza (Cheese, Peperoni, Hawaiian): ")
+
+if Type == "Cheese"
+	print OnePoint
+elif Type == "Peperoni"
+	print TwoPoint
+elif Type == "Hawaiian"
+	print ThreePoint
+def GuessinSize():
+	Size= raw_input("Choose the type of your pizza (Large, Medium, Small): ")
+   
 
