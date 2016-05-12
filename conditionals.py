379e1f93c52a31093ced9a42ae8cf37a9254b25b
cmis <cmis@cmis-hs01.(none)>
2016-05-12 11:55:30 +0700

    Saved

--- conditionals.py
+++ conditionals.py
@@ -7,50 +7,68 @@ print "Holly macaroni! You are trapped in a Pizza Restaurant! If you don't want 
 
 def add(a, b):
 	return float(a) + float(b)
-
+#Shape
 def GuessingShape():
 	Shape = raw_input("Choose ONE type of triangles of your pizza,(Isoceles, Scalene, Right) :")
 	if Shape == "Scalene":
 		print float(1)
+		return float(1)
 	elif Shape == "Isoceles":
 		print float(2)
+		return float(2)
+		
 	elif Shape == "Right":
-	    print float(3)
-GuessingShape()
+		print float(3)
+		return float(3)
+		
 #Types
 def GuessingType():
 	Type= raw_input("Choose the type of of your pizza (Cheese, Peperoni, Hawaiian): ")
 	if Type == "Cheese":
 		print float(1)
+		return float(1)
 	elif Type == "Peperoni":
 		print float(2)
+		return float(2)
 	elif Type == "Hawaiian":
 		print float(3)
-GuessingType()
-#Sizes
+		return float(3)
+		
+#Sizes	
 def GuessingSize():
 	Size= raw_input("Choose the size of your pizza (Large, Medium, Small): ")
 	if Size == "Large":
 		print float(1)
+		return float(1)		
 	elif Size == "Medium":
 		print float(2)
+		return float(2)
 	elif Size== "Small":
 		print float(3)
-GuessingSize()
-
+		return float(3)
+#Bonus
+def Bonus():
+	Bonus = raw_input("Guess a number between 1 and 9 for a bonus chance to fix your score. ")
+	Answer = random.randint(0,9)
+	if int(Bonus) == Answer:
+		print Bonus
+	else:
+		print "No bonus for you you idiot." 
+			
 #Calculation of The Points
-
 def main():
-	return GuessingShape()
-	return GuessingType()
-	return GuessingSize()
-	YourScore = GuessinShape +GuessinType+ GuessinSize
+	Shape=GuessingShape()
+	Type= GuessingType()
+	Size=GuessingSize()
+	YourScore = Shape + Type + Size
+
 	output= """
 Your Score is {}
 Try Again.
 """.format(YourScore)
+	print Bonus()
 	if int(YourScore) == int(3):
-		print "Good JOb!"
+		print "Good Job!"
 	elif int(YourScore) == int(6):
 		print "You're Out!"
 	elif int(YourScore) == int(9):
@@ -58,13 +76,15 @@ Try Again.
 #Conclusion
 	elif int(YourScore) < int(3) or int(YourScore) > int(3):
 		print"BOOO HOOO"
+		print output
 	elif int(YourScore) < int(6) or int(YOurScore) > int(6):
 		print"FAIL"
-	elif int(YourScore)< int(9) or int(YourScore) > int(9):
-		print"GAME OVERR"
 	else:
-		print output
+		print"GAME OVERR"
+
+		
 main()
+
 	
 
 
