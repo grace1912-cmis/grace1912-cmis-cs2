f9d866bba93e0878e65a92fb8af28f67d2512ada
cmis <cmis@cmis-hs00.(none)>
2016-05-24 09:55:27 +0700

    Saved

---
+++ WhileLoops.py
@@ -0,0 +1,18 @@
+#def CountDown(x):
+#	while 0 < x:
+#		print x
+#		x-=1
+#CountDown(12)
+
+#def Counter(x):
+#	while 0 > x:
+#		print x
+#		x+=1
+#	while 0 < x:
+#		print x
+#		x-=1
+#Counter(-10)
+#Counter(10)
+
+def Addup(x):
+			
