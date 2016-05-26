7405287fd24e31ba1fc2b3ed422dbe07c9d743a2
cmis <cmis@cmis-hs00.(none)>
2016-05-26 14:36:38 +0700

    saved

--- WhileLoops.py
+++ WhileLoops.py
@@ -14,5 +14,27 @@
 #Counter(-10)
 #Counter(10)
 
-def Addup(x):
-			
+#def CountFrom(x,y):
+#	while x >= y:
+#		print x
+#		x-=1
+#	while x <= y:
+#		print x
+#		x+=1
+#CountFrom(5,0)
+#CountFrom(0,5)
+
+def SumOfOdds(n):
+	while n > 0:
+		if n % 2 == 1:	
+			n -= 1
+		
+	while n < 0:
+		return n
+		n+= (n % 2 == 1)
+SumOfOdds(10)	
+
+
+
+
+		
