915fa083e1355d048c3f6e1864196800b71e49ab
cmis <cmis@cmis-hs00.(none)>
2016-05-30 09:55:27 +0700

    commit

--- WhileLoops.py
+++ WhileLoops.py
@@ -24,17 +24,29 @@
 #CountFrom(5,0)
 #CountFrom(0,5)
 
-def SumOfOdds(n):
-	while n > 0:
-		if n % 2 == 1:	
-			n -= 1
-		
-	while n < 0:
-		return n
-		n+= (n % 2 == 1)
-SumOfOdds(10)	
+#def SumOfOdds(n):
+#	if n > 0:
+#		while n > 0:
+#			if n % 2 == 1:
+#				total += n
+#			n -= 1
+#	elif n < 0:
+#		while n < 0:
+#			if n % 2 == 1:
+#				total += n
+#			n+ = 1
+#	return total
+#print SumOfOdds(10)
 
-
-
+def Grid(w, h):
+	out = ""
+	while h > 0:
+		while w > 0:
+			out += "."
+			w -= 1
+		out += "/n"
+		h-= 1
+		return out	
+print Grid(5, 2)
 
 		
